d57bf9fb22fc50b5f478dbb0f284247bc7dc8e90
Ian Lum <ianlum314@gmail.com>
2024-05-29 13:34:56 -0700

    mouse mode changes

--- player.gd
+++ player.gd
@@ -1,15 +1,16 @@
 extends RigidBody3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("left", "right")
 	input.z = Input.get_axis("forward", "back")
+	
 	apply_central_force(input * 1200.0 * delta)
 	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
