abc0e2ef4154af8bd842957bc23b95e3d2af49ce
Ian Lum <ianlum314@gmail.com>
2024-05-29 14:44:19 -0700

    swap to characterbody3d

--- player.gd
+++ player.gd
@@ -1,11 +1,13 @@
-extends RigidBody3D
+extends CharacterBody3D
+
 
 const MOUSE_SENSITIVITY = 0.001
-const SPEED = 1200.0
-const JUMP_FORCE = 400.0
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
 var twist_input := 0.0
 var pitch_input := 0.0
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var twist_pivot = $TwistPivot
 @onready var pitch_pivot = $TwistPivot/PitchPivot
@@ -13,13 +15,20 @@ var pitch_input := 0.0
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
-func _process(delta):
-	var input := Vector3.ZERO
-	input.x = Input.get_axis("left", "right")
-	input.z = Input.get_axis("forward", "back")
-	
-	apply_central_force(twist_pivot.basis * input * SPEED * delta)
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (twist_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
 	
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
@@ -32,12 +41,13 @@ func _process(delta):
 	twist_input = 0
 	pitch_input = 0
 
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			twist_input = -event.relative.x * MOUSE_SENSITIVITY
 			pitch_input = -event.relative.y * MOUSE_SENSITIVITY
-	elif event.is_action_pressed("jump"):
-		apply_central_force(Vector3.UP * JUMP_FORCE)
-	elif event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
