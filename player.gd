381ed005f54b4b5c6218a5971a26c72709f4465e
Ian Lum <ianlum314@gmail.com>
2024-05-29 13:47:03 -0700

    add camera movement

--- player.gd
+++ player.gd
@@ -1,5 +1,13 @@
 extends RigidBody3D
 
+const MOUSE_SENSITIVITY = 0.001
+const SPEED = 1800.0
+
+var twist_input := 0.0
+var pitch_input := 0.0
+
+@onready var twist_pivot = $TwistPivot
+@onready var pitch_pivot = $TwistPivot/PitchPivot
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -10,7 +18,24 @@ func _process(delta):
 	input.x = Input.get_axis("left", "right")
 	input.z = Input.get_axis("forward", "back")
 	
-	apply_central_force(input * 1200.0 * delta)
+	apply_central_force(twist_pivot.basis * input * SPEED * delta)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(
+		pitch_pivot.rotation.x,
+		deg_to_rad(-30),
+		deg_to_rad(30)
+	)
+	
+	twist_input = 0
+	pitch_input = 0
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			twist_input = -event.relative.x * MOUSE_SENSITIVITY
+			pitch_input = -event.relative.y * MOUSE_SENSITIVITY
