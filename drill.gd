d3793732ae3928831b5501e14f48a82e17ea5db2
Ian Lum <ianlum314@gmail.com>
2024-06-05 13:19:50 -0700

    drill sticks and becomes climbable

--- drill.gd
+++ drill.gd
@@ -1,7 +1,13 @@
 extends RigidBody3D
 
 const SPEED = 800
-var direction: Vector3 = Vector3.FORWARD
+var direction: Vector3
 
 func _ready():
+	#look_at(global_position + direction)
+	rotation.y = atan2(direction.x, direction.z)
 	apply_central_force(direction * SPEED)
+
+func _on_body_entered(body):
+	freeze = true
+	set_collision_layer_value(1, true)
