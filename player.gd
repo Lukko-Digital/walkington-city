b89c38d62e13a8e071077fadf79ce157af9c91d9
Ian Lum <ianlum314@gmail.com>
2024-06-05 13:25:32 -0700

    refactoring

--- player.gd
+++ player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 const MOUSE_SENSITIVITY = 0.001
 const CONTROLLER_SENSITIVITY = 0.05
 const SPEED = 5.0
@@ -12,44 +11,44 @@ var pitch_input := 0.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var drill_scene = preload("res://drill.tscn")
-@onready var twist_pivot = $TwistPivot
-@onready var pitch_pivot = $TwistPivot/PitchPivot
+@onready var camera_twist = $TwistPivot
+@onready var camera_pitch = $TwistPivot/PitchPivot
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
+	handle_movement(delta)
+	handle_camera()
+
+func handle_movement(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta * 2
 
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
-	var direction = (twist_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (camera_twist.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		
 	move_and_slide()
-	
-	handle_camera()
 
 func handle_camera():
 	#twist_input = Input.get_axis("look_right", "look_left") * CONTROLLER_SENSITIVITY
 	#pitch_input = Input.get_axis("look_down", "look_up") * CONTROLLER_SENSITIVITY * 0.5
 	
-	twist_pivot.rotate_y(twist_input)
-	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(
-		pitch_pivot.rotation.x,
+	camera_twist.rotate_y(twist_input)
+	camera_pitch.rotate_x(pitch_input)
+	camera_pitch.rotation.x = clamp(
+		camera_pitch.rotation.x,
 		deg_to_rad(-CAMERA_ANGLE_LIMIT),
 		deg_to_rad(CAMERA_ANGLE_LIMIT)
 	)
 	twist_input = 0
 	pitch_input = 0
-	
-	#print($TwistPivot/PitchPivot/Camera3D.global_transform)
 
 func throw_drill():
 	var instance = drill_scene.instantiate()
