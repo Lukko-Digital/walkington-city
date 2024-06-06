5cce6d72a126aa98f381a9d316d9f77fcdb4a620
Ian Lum <ianlum314@gmail.com>
2024-06-06 16:21:21 -0700

    add knight movement

---
+++ knight.gd
@@ -0,0 +1,67 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const ACCELERATION = 4.0
+const JUMP_VELOCITY = 8.0
+const ROTATION_SPEED = 12.0
+const MOUSE_SENSITIVITY = 0.0015
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var jumping = false
+var attacks = [
+	"Attack_Diagonal",
+	"Attack_Chop",
+	"Attack_Horizontal"
+]
+var previously_grounded = true
+
+@onready var spring_arm = $SpringArm3D
+@onready var model = $Rig
+@onready var anim_tree = $AnimationTree
+@onready var anim_state: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	velocity.y -= gravity * delta
+	get_move_input(delta)
+	
+	move_and_slide()
+	if velocity.length() > 1.0:
+		model.rotation.y = lerp_angle(model.rotation.y, spring_arm.rotation.y, ROTATION_SPEED * delta)
+	if (
+		is_on_floor() and
+		Input.is_action_just_pressed("jump") and
+		anim_state.get_current_node() != "Jump_Land"
+	):
+		velocity.y = JUMP_VELOCITY
+		jumping = true
+		anim_tree.set("parameters/conditions/jumping", true)
+		anim_tree.set("parameters/conditions/grounded", false)
+	if is_on_floor() and not previously_grounded:
+		jumping = false
+		anim_tree.set("parameters/conditions/jumping", false)
+		anim_tree.set("parameters/conditions/grounded", true)
+	if not is_on_floor() and not jumping:
+		anim_state.travel("Jump_Idle")
+		anim_tree.set("parameters/conditions/grounded", false)
+	previously_grounded = is_on_floor()
+
+func get_move_input(delta):
+	var vy = velocity.y
+	velocity.y = 0
+	var input = Input.get_vector("left", "right", "forward", "back")
+	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
+	velocity = velocity.lerp(dir * SPEED, ACCELERATION * delta)
+	var vl = velocity * model.transform.basis
+	anim_tree.set("parameters/IdleWalkRun/blend_position", Vector2(vl.x, -vl.z) / SPEED)
+	velocity.y = vy
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		spring_arm.rotation.x -= event.relative.y * MOUSE_SENSITIVITY
+		spring_arm.rotation_degrees.x = clamp(spring_arm.rotation_degrees.x, -90.0, 30.0)
+		spring_arm.rotation.y -= event.relative.x * MOUSE_SENSITIVITY
+	if event.is_action_pressed("throw"):
+		anim_state.travel(attacks.pick_random())
